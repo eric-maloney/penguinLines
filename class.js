d3228e80ca5858c455bcc973ebc16c72e4fe0afc
unknown <eric,maloney@centre.edu>
2020-04-21 00:51:24 -0400

    linelabfinal

--- class.js
+++ class.js
@@ -1,9 +1,9 @@
 var studentPromise= d3.json("classData.json");
 studentPromise.then (function(penguins){
     console.log(penguins);
-    
 
-   initGraph("#graph")
+    
+   initGraph(penguins);
     
 },function(err)
 {
@@ -12,35 +12,66 @@ studentPromise.then (function(penguins){
 
 
 
-var drawLines = function(penguins,target,graph,xScale,yScale)
+var drawLines = function(penguins)
 {
+    
+    var xScale = d3.scaleLinear()
+        .domain([0,penguins[0].quizes.length-1])
+        .range([0,graph.width])
+    
+    
+    var lowGrade = d3.min(penguins,function(penguin)
+    {
+        return d3.min(penguin.quizes,
+                      function(quiz,index)
+                     {
+                        return quiz[0];
+                     });
+    })
+    
+    var highGrade = d3.max(penguins,function(penguin)
+    {   
+        return d3.max(penguin.quizes,
+                      function(quiz,index)
+                     {
+                        return quiz[0];
+                     });
+    })
+    
+    var yScale = d3.scaleLinear()
+        .domain([lowGrade,highGrade])
+        .range([graph.height,0])
+    
+    
     
     var lineGenerator = d3.line()
         .x(function(quiz,i) 
         { 
-            return xScale(i);
+           
+            return xScale(quiz.day);
         })
         .y(function(quiz)   
         { 
+            
             return yScale(quiz.grade);
         })
         .curve(d3.curveCardinal)
     
-    
         
     var svg = d3.select("body")                .append("svg")              
-    .attr("width", 50)                
-    .attr("height", 100);
+    .attr("width", 550)                
+    .attr("height", 300);
     
+    
+    console.log(penguins[0].quizes)
     svg.append("path")       
-        .datum(quizes)       
+        .datum(penguins)       
         .attr("class", "line")       
-        .attr("d", line); 
+        .attr("d",lineGenerator); 
     
     
     
-    var lines = d3.select(target)
-        .select("#graph")
+    var lines = d3.select("#graph")
         .selectAll("g")
         .data(penguins)
         .enter()
@@ -50,7 +81,7 @@ var drawLines = function(penguins,target,graph,xScale,yScale)
             return penguin.quizes.grade;
         })
         .classed("line",true)
-        .attr("fill","none")
+        .attr("fill","black")
         .attr("stroke","black")
         .attr("stroke-width",3)
         .on("mouseover",function(penguin)
@@ -82,14 +113,18 @@ var drawLines = function(penguins,target,graph,xScale,yScale)
         return penguin.quizes;
             })
         .attr("d",lineGenerator)
+    
+    
+    
+    
 }
 
 
 
 
-var createLabels = function(screen,margins,graph,target)
+var createLabels = function(screen,margins,graph)
 {
-        var labels = d3.select(target)
+        var labels = d3.select("#graph")
         .append("g")
         .classed("labels",true)
         
@@ -119,13 +154,13 @@ var createLabels = function(screen,margins,graph,target)
 }
 
 
-var createAxes = function(screen,margins,graph,
-                           target,xScale,yScale)
+var createAxes = function(screen,margins,graph,xScale,yScale)
 {
+     
     var xAxis = d3.axisBottom(xScale);
     var yAxis = d3.axisLeft(yScale);
     
-    var axes = d3.select(target)
+    var axes = d3.select("#graph")
         .append("g")
     axes.append("g")
         .attr("transform","translate("+margins.left+","
@@ -138,7 +173,7 @@ var createAxes = function(screen,margins,graph,
 }
 
 
-var initGraph = function(target,penguins)
+var initGraph = function(penguins)
 {
     
     var screen = {width:550, height:300};
@@ -154,16 +189,41 @@ var initGraph = function(target,penguins)
     }
     
     
-    d3.select(target)
+    d3.select("#graph")
         .attr("width",screen.width)
         .attr("height",screen.height)
     
-    var g = d3.select(target)
+    var g = d3.select("graph")
         .append("g")
         .classed("graph",true)
         .attr("transform","translate("+margins.left+","+
              margins.top+")");
         
+    
+
+   
+var tooltip = d3.select("#graph svg")
+.append("img")
+.style("opacity", 0)
+.attr("class","tooltip")
+
+var mouseover = function(penguin)
+{
+    tooltip
+    .style("opacity",0)
+    .append("img")
+    .attr("src",function(penguin)
+      {
+    return "imgs/" + penguin.picture;
+});
+}
+
+var mouseleave= function(penguin)
+{
+ tooltip
+ .style("opacity",0)
+    
+}
     
     
     var xScale = d3.scaleLinear()
@@ -176,7 +236,7 @@ var initGraph = function(target,penguins)
         return d3.min(penguin.quizes,
                       function(quiz,index)
                      {
-                        return quiz-arr[0];
+                        return quiz[0];
                      });
     })
     
@@ -185,7 +245,7 @@ var initGraph = function(target,penguins)
         return d3.max(penguin.quizes,
                       function(quiz,index)
                      {
-                        return quiz-arr[0];
+                        return quiz[0];
                      });
     })
     
@@ -193,13 +253,12 @@ var initGraph = function(target,penguins)
         .domain([lowGrade,highGrade])
         .range([graph.height,0])
     
-   
+    createLabels(screen,margins,graph);
+    createAxes(screen,margins,graph,xScale,yScale);
+    console.log(penguins);
+    drawLines(penguins)
     
     
-     createLabels(screen,margins,graph,target);
-    createAxes(screen,margins,graph,target,xScale,yScale);
-    drawLines(penguins,graph,target,
-              xScale,yScale);
     
 }
 
